78baaaf57a1680cfc8035f2605541170021dbaf4
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-09-19 13:20:21 -0700

    Done.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,4 @@
 class PlantsController < ApplicationController
-rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -9,7 +8,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants/:id
   def show
-    plant = find_plant
+    plant = Plant.find_by(id: params[:id])
     render json: plant
   end
 
@@ -18,18 +17,18 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
-  
-  # PATCH /plants
+
+  # PATCH /plants/:id
   def update
     plant = find_plant
     plant.update(plant_params)
     render json: plant
   end
 
-  # DELETE /plants/:id
+  # DELETE /plants/:id 
   def destroy
     plant = find_plant
-    plant.destroy
+    plant.destroy 
     head :no_content
   end
 
@@ -38,7 +37,7 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
-  
+
   def find_plant
     Plant.find(params[:id])
   end
